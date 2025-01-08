b870a867f8325a1ae58e87622ef0040850dc2cae
Emma Larsson <emma940112@gmail.com>
2025-01-08 01:09:56 +0100

    Startat ett nytt vue-projekt

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+        <nav>
+        <RouterLink to="/">Startsida</RouterLink>
+        <RouterLink to="/data">Data</RouterLink>
+        <RouterLink to="/about">Om Vue.js</RouterLink>
+      </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
